6464dbf6b0c7964fd7ec3418713e9ac82194ce12
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-03-26 00:41:17 +0000

    Remove SQL things, alembic

--- src/config/session.py
+++ src/config/session.py
@@ -1,45 +1,12 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
-from alembic import command, config
-from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
-from sqlalchemy import Connection
-from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker
-from sqlmodel import create_engine
-from sqlmodel.ext.asyncio.session import AsyncSession
+from neo4j import (
+    AsyncGraphDatabase,
+    AsyncSession as Neo4jAsyncSession,
+)
 
 from src.config.settings import settings
-
-from neo4j import AsyncGraphDatabase, AsyncTransaction, AsyncResult, AsyncSession as Neo4jAsyncSession
-
-# from motor.motor_asyncio import AsyncIOMotorClient
-# from odmantic import AIOEngine
-
-postgres_engine = AsyncEngine(create_engine(settings.POSTGRES_URI, future=True))
-
-# _mongo_client: AsyncIOMotorClient = AsyncIOMotorClient(settings.MONGO_URI)
-# mongo_engine = AIOEngine(client=_mongo_client, database=settings.MONGO_DB)
-
-SQLAlchemyInstrumentor().instrument(engine=postgres_engine.sync_engine)
-
-
-def run_postgres_upgrade(connection: Connection, cfg: config.Config) -> None:
-    cfg.attributes["connection"] = connection
-    command.upgrade(cfg, "head")
-
-
-async def init_postgres_db() -> None:
-    async with postgres_engine.begin() as conn:
-        await conn.run_sync(run_postgres_upgrade, config.Config("alembic.ini"))
-
-
-@asynccontextmanager
-async def get_postgres_session() -> AsyncGenerator[AsyncSession, None]:
-    async_session = async_sessionmaker(
-        postgres_engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with async_session() as session:
-        yield session
 
 
 class Neo4jSession:
@@ -47,7 +14,7 @@ class Neo4jSession:
         self.driver = AsyncGraphDatabase.driver(
             settings.NEO4J_URL,
             auth=(settings.NEO4J_USER, settings.NEO4J_PASSWORD),
-            database=settings.NEO4J_DB
+            database=settings.NEO4J_DB,
         )
 
     @asynccontextmanager
